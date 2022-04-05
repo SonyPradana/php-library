05fd369709b10cec092dc12372d877adf6aa24f2
Angger Pradana <sonypradana@gmail.com>
2022-04-05 22:27:36 +0700

    feat: add method close() to closed middleware

--- src/System/Router/AbstractMiddleware.php
+++ src/System/Router/AbstractMiddleware.php
@@ -4,7 +4,18 @@ namespace System\Router;
 
 abstract class AbstractMiddleware
 {
+  /**
+   * Run middleware.
+   */
   public function handle()
+  {
+    // ovveridedable
+  }
+
+  /**
+   * Use for clear middleware if needed.
+   */
+  public function close()
   {
     // ovveridedable
   }
