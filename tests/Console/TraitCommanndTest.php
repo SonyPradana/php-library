57dcdc113994d8ead91594d0ab4d329714ed8669
anggerpradana <75710827+anggerpradana@users.noreply.github.com>
2022-07-02 11:24:32 +0700

    More Commandline Color Palette (#28)
    * fix: wrong console color code
    
    * feat: more command color palette
    
    - text color palette cyan, magenta, light (cyan magenta red yellow blue)
    - bg color palette cyan, magenta, light (cyan magenta red yellow blue)
    - added just in time text and backgroud color
    - added bold, underline, reverse, hidden

---
+++ tests/Console/TraitCommanndTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use System\Console\Command;
+
+final class TraitCommandTest extends TestCase
+{
+    /** @var class */
+    private $command;
+
+    protected function setUp(): void
+    {
+        $this->command = new class(['cli', '--test']) extends Command {
+            public function __call($name, $arguments)
+            {
+                if ($name === 'echoTextRed') {
+                    echo $this->textRed('Color');
+                }
+                if ($name === 'echoTextYellow') {
+                    echo $this->textYellow('Color');
+                }
+                if ($name === 'echoTextGreen') {
+                    echo $this->textGreen('Color');
+                }
+                if ($name === 'textColor') {
+                    echo $this->textColor((int) $arguments[0], 'Color');
+                }
+            }
+        };
+    }
+
+    /** @test */
+    public function itCanMakeTextRed()
+    {
+        ob_start();
+        $this->command->echoTextRed();
+        $out = ob_get_clean();
+
+        $this->assertEquals("\e[31mColor\e[0m", $out);
+    }
+
+    /** @test */
+    public function itCanMakeTextYellow()
+    {
+        ob_start();
+        $this->command->echoTextYellow();
+        $out = ob_get_clean();
+
+        $this->assertEquals("\e[33mColor\e[0m", $out);
+    }
+
+    /** @test */
+    public function itCanMakeTextGreen()
+    {
+        ob_start();
+        $this->command->echoTextGreen();
+        $out = ob_get_clean();
+
+        $this->assertEquals("\e[32mColor\e[0m", $out);
+    }
+
+    /** @test */
+    public function itCanMakeTextColor()
+    {
+        ob_start();
+        $this->command->textColor(25);
+        $out = ob_get_clean();
+
+        $this->assertEquals("\e[38;5;25mColor\e[0m", $out);
+    }
+}
