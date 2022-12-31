c40a5ac592dc97b553eb8dd3f64ca7715217f946
Angger Pradana <sonypradana@gmail.com>
2022-12-31 17:48:53 +0700

    add more test

--- tests/DataBase/Query/SelectTest.php
+++ tests/DataBase/Query/SelectTest.php
@@ -124,4 +124,49 @@ final class SelectTest extends \QueryStringTest
             'select with where statment is like'
         );
     }
+
+      /** @test */
+      public function itCorrectSelectMultyColumn(): void
+      {
+          $select = MyQuery::from('test', $this->PDO)
+              ->select(['column_1', 'column_2', 'column_3'])
+              ->equal('column_1', 123)
+              ->equal('column_2', 'abc')
+              ->equal('column_3', true);
+
+          $this->assertEquals(
+              'SELECT `column_1`, `column_2`, `column_3` FROM `test` WHERE ( (test.column_1 = :column_1) AND (test.column_2 = :column_2) AND (test.column_3 = :column_3) )',
+              $select->__toString(),
+              'select statment must have 3 selected query'
+          );
+
+          //   WARNING: bolean return as boolean not 0 or 1
+          //   $this->assertEquals(
+        //       "SELECT `column_1`, `column_2`, `column_3` FROM `test` WHERE ( (test.column_1 = 123) AND (test.column_2 = 'abc') AND (test.column_3 = 1) )",
+        //       $select->queryBind(),
+        //       'select statment must have 3 selected query'
+          //   );
+      }
+
+    /** @test */
+    public function itCorrectSelectWithStrictOff(): void
+    {
+        $select = MyQuery::from('test', $this->PDO)
+            ->select(['column_1', 'column_2', 'column_3'])
+            ->equal('column_1', 123)
+            ->equal('column_2', 'abc')
+            ->strictMode(false);
+
+        $this->assertEquals(
+            'SELECT `column_1`, `column_2`, `column_3` FROM `test` WHERE ( (test.column_1 = :column_1) OR (test.column_2 = :column_2) )',
+            $select,
+            'select statment must have using or statment'
+        );
+
+        $this->assertEquals(
+            "SELECT `column_1`, `column_2`, `column_3` FROM `test` WHERE ( (test.column_1 = 123) OR (test.column_2 = 'abc') )",
+            $select->queryBind(),
+            'select statment must have using or statment'
+        );
+    }
 }
