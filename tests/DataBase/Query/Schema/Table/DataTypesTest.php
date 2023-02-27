526a3d2fcbd437059d3fce897c52b3881b46b5c0
Angger Pradana <sonypradana@gmail.com>
2023-02-26 22:49:34 -0800

    feat: add enum datatype (#129)

---
+++ tests/DataBase/Query/Schema/Table/DataTypesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace System\Test\Database\Query\Schema\Table;
+
+use System\Database\MySchema\Table\Create;
+
+final class DataTypesTest extends \QueryStringTest
+{
+    /** @test */
+    public function itCanGenerateQueryUsingAddColumn()
+    {
+        $schema = new Create('testing_db', 'test', $this->pdo_schame);
+        $schema('name')->varchar(40);
+        $schema('size')->enum(['x-small', 'small', 'medium', 'large', 'x-large']);
+
+        $this->assertEquals(
+            "CREATE TABLE testing_db.test ( `name` varchar(40), `size` ENUM ('x-small', 'small', 'medium', 'large', 'x-large') )",
+            $schema->__toString()
+        );
+    }
+}
