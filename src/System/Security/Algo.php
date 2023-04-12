cee401ae3f220de57ffbaefe037d417926dac0d8
Angger Pradana <sonypradana@gmail.com>
2023-04-12 12:06:00 +0700

    [Feat] Add Support encrypt decrypt (#143)
    * feat: add support encrypt decrypt
    
    * test: merge test
    
    * feat: `Crypt` alogo parse

---
+++ src/System/Security/Algo.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace System\Security;
+
+class Algo
+{
+    public const AES_128_CBC = 'aes-256-cbc;16';
+    public const AES_256_CBC = 'aes-256-cbc;16';
+}
---
+++ src/System/Security/helper.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace System\Security;
+
+if (!function_exists('encrypt')) {
+    function encrypt(
+        string $plain_text,
+        string $passphrase = null,
+        string $algo = Algo::AES_256_CBC
+    ): string {
+        return (new Crypt($passphrase, $algo))->encrypt($plain_text);
+    }
+}
+
+if (!function_exists('decrypt')) {
+    function decrypt(
+        string $encrypted,
+        string $passphrase = null,
+        string $algo = Algo::AES_256_CBC
+    ): string {
+        return (new Crypt($passphrase, $algo))->decrypt($encrypted);
+    }
+}
