f16321a59a1dcba07d6df1c4bc3a9465601134ca
SonyPradana <sonypradana@gmail.com>
2023-04-27 22:00:29 +0700

    feat: support text alignment

--- src/System/Console/Style/Box.php
+++ src/System/Console/Style/Box.php
@@ -6,13 +6,19 @@ namespace System\Console\Style;
 
 use System\Console\Interfaces\Border;
 use System\Console\Interfaces\BoxSize;
+use System\Text\Str;
 
 class Box
 {
+    public const LEFT   = 0;
+    public const RIGHT  = 1;
+    public const CENTER = 2;
+
     private string $text;
     private BoxSize $boxSize;
     private Border $border;
-    private int $left = 0;
+    private int $left      = 0;
+    private int $alignment = 0;
 
     public function __construct(
         string $text,
@@ -27,6 +33,13 @@ class Box
     public function left(int $left): self
     {
         $this->left = $left;
+
+        return $this;
+    }
+
+    public function textAlignment(int $alignment): self
+    {
+        $this->alignment = $alignment;
 
         return $this;
     }
@@ -74,8 +87,9 @@ class Box
             ->repeat(' ', $this->boxSize->margin()->left())
             ->push($this->border->corner()[3])
             ->textColor($this->border->color())
-            ->repeat('─', $this->boxSize->width() - 2)
+            ->repeat('─', $this->boxSize->width() + $this->boxSize->padding()->left() + $this->boxSize->padding()->right())
             ->textColor($this->border->color())
+
             ->push($this->border->corner()[0])
             ->textColor($this->border->color())
             ->repeat(' ', $this->boxSize->margin()->right())
@@ -89,41 +103,41 @@ class Box
                 ->repeat(' ', $this->boxSize->margin()->left())
                 ->push('│')
                 ->textColor($this->border->color())
-                ->repeat(' ', $this->boxSize->width() - 2)
+                ->repeat(' ', $this->boxSize->width() + $this->boxSize->padding()->left() + $this->boxSize->padding()->right())
                 ->push('│')
                 ->textColor($this->border->color())
                 ->new_lines()
             ;
         }
-        $content
-            ->repeat(' ', $this->left)
-            ->repeat(' ', $this->boxSize->margin()->left())
-        ;
-        $l          = $this->boxSize->width() - strlen($this->text) - ($this->boxSize->padding()->left() + $this->boxSize->padding()->right()) - 2;
-        $l          = $l < 0 ? 0 : $l;
-        $paragraf[] = $this->text . str_repeat(' ', $l);
-        foreach ($paragraf as $p) {
+        foreach ($this->alignment($this->text, $this->alignment, $this->boxSize->width()) as $key => $value) {
+            if ($key + 1 > $this->boxSize->height()) {
+                $value = trim($value) . '...';
+                $value = $value . str_repeat(' ', $this->boxSize->width() - strlen($value));
+            }
             $content
+                ->repeat(' ', $this->left)
+                ->repeat(' ', $this->boxSize->margin()->left())
                 ->push('│')
                 ->textColor($this->border->color())
                 ->repeat(' ', $this->boxSize->padding()->left())
-                ->push($p)
+                ->push($value)
                 ->repeat(' ', $this->boxSize->padding()->right())
                 ->push('│')
                 ->textColor($this->border->color())
+                ->repeat(' ', $this->boxSize->margin()->right())
                 ->new_lines()
             ;
+            if ($key + 1 > $this->boxSize->height()) {
+                break;
+            }
         }
-        $content
-            ->repeat(' ', $this->boxSize->margin()->right())
-        ;
         foreach (range(0, $this->boxSize->padding()->bottom()) as $p) {
             $content
                 ->repeat(' ', $this->left)
                 ->repeat(' ', $this->boxSize->margin()->left())
                 ->push('│')
                 ->textColor($this->border->color())
-                ->repeat(' ', $this->boxSize->width() - 2)
+                ->repeat(' ', $this->boxSize->width() + $this->boxSize->padding()->left() + $this->boxSize->padding()->right())
                 ->push('│')
                 ->textColor($this->border->color())
                 ->new_lines()
@@ -136,7 +150,7 @@ class Box
             ->repeat(' ', $this->boxSize->margin()->left())
             ->push($this->border->corner()[2])
             ->textColor($this->border->color())
-            ->repeat('─', $this->boxSize->width() - 2)
+            ->repeat('─', $this->boxSize->width() + $this->boxSize->padding()->left() + $this->boxSize->padding()->right())
             ->textColor($this->border->color())
             ->push($this->border->corner()[1])
             ->textColor($this->border->color())
@@ -151,5 +165,56 @@ class Box
             'border_bottom' => $border_bottom,
             'margin_bottom' => $margin_top,
         ];
+    }
+
+    private function alignment(string $text, int $alignment, int $width)
+    {
+        $words = explode(' ', $text);
+        $lines = [];
+        $line  = 0;
+        while (count($words) > 0) {
+            if (!array_key_exists($line, $lines)) {
+                $lines[$line] = array_shift($words);
+                continue;
+            }
+            $word   = array_shift($words);
+            $lenght = strlen($word) + strlen($lines[$line]) + 1;
+            if ($lenght < $width) {
+                $lines[$line] .= ' ' . $word;
+                continue;
+            }
+            $line++;
+            $lines[$line] = $word;
+        }
+
+        if ($alignment === static::RIGHT) {
+            return array_map(
+                fn ($value) => Str::fill($value, ' ', $width),
+                $lines
+            );
+        }
+
+        if ($alignment === static::CENTER) {
+            return array_map(
+                // fn($value) => Str::fill($value, ' ', $width),
+                function ($value) use ($width) {
+                    foreach (range(1, $width - strlen($value)) as $q) {
+                        if ($q % 2 === 0) {
+                            $value = ' ' . $value;
+                        } else {
+                            $value .= ' ';
+                        }
+                    }
+
+                    return $value;
+                },
+                $lines
+            );
+        }
+
+        return array_map(
+            fn ($value) => Str::fillEnd($value, ' ', $width),
+            $lines
+        );
     }
 }
