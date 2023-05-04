0f12319f8494edfcabe5c187a16cee5227e6d19d
SonyPradana <sonypradana@gmail.com>
2023-05-04 23:15:02 +0700

    feat: get item using dot notation

--- src/System/Collection/helper.php
+++ src/System/Collection/helper.php
@@ -37,3 +37,38 @@ if (!function_exists('collection_immutable')) {
         return new Collection($collection);
     }
 }
+
+if (!function_exists('data_get')) {
+    /**
+     * Get array-value using dot notation.
+     *
+     * @param array<string, mixed> $array
+     * @param string               $key
+     * @param mixed                $default
+     *
+     * @return mixed|array<string, mixed>|null
+     */
+    function data_get($array, $key, $default = null)
+    {
+        $segments = explode('.', $key);
+        foreach ($segments as $segment) {
+            if (array_key_exists($segment, $array)) {
+                $array = $array[$segment];
+            } elseif ('*' === $segment) {
+                $values = [];
+                foreach ($array as $item) {
+                    $value = data_get($item, implode('.', array_slice($segments, 1)));
+                    if (null !== $value) {
+                        $values[] = $value;
+                    }
+                }
+
+                return count($values) > 0 ? $values : $default;
+            } else {
+                return $default;
+            }
+        }
+
+        return $array;
+    }
+}
