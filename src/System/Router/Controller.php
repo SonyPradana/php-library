4cf7e53bad2820c546cc2ed3eb6a9270461ff513
SonyPradana <sonypradana@gmail.com>
2023-05-10 22:40:03 +0700

    feat: __invoke now using single parameter

--- src/System/Router/Controller.php
+++ src/System/Router/Controller.php
@@ -6,10 +6,15 @@ use System\Http\Response;
 
 abstract class Controller
 {
-    public function __invoke(string $invoke, array $parameter = []): Response
+    public function __invoke(...$args): Response
     {
-        return call_user_func([$this, $invoke], $parameter);
+        return $this->callSelf($args['method'], $args['parameter']);
     }
 
     abstract public static function renderView(string $view_path, array $portal = [], int $status_code = Response::HTTP_OK, array $headers = []): Response;
+
+    public function callSelf(string $method, array $parameter): Response
+    {
+        return call_user_func([$this, $method], array_values($parameter));
+    }
 }
