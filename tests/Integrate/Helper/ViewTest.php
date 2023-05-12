c5351a9e8b8aa40dc67dc493c56fd4dcdd68da65
Angger Pradana <sonypradana@gmail.com>
2023-05-12 13:01:52 +0700

    [Feat] Add View Helper (#157)
    * feat: add view helper
    
    * test: add git ignore to keep floder clean
    
    * feat: add aditional options
    
    * refactor: use animus function call view render
    
    * feat: add three parameter options

---
+++ tests/Integrate/Helper/ViewTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace System\Test\Integrate\Helper;
+
+use PHPUnit\Framework\TestCase;
+use System\Http\Response;
+use System\Integrate\Application;
+use System\Text\Str;
+use System\View\Templator;
+
+final class ViewTest extends TestCase
+{
+    public function testItCanGetResponeFromeContainer()
+    {
+        $app = new Application('/');
+
+        $app->set(
+            'view.response',
+            fn () => fn (string $view_path, array $portal = []): Response => (new Response())
+                        ->setContent(
+                            (new Templator(__DIR__ . '/assets/view/', __DIR__ . '/assets/cache'))
+                                ->render($view_path, $portal)
+                        )
+        );
+
+        $view = view('test.php', [], ['status' => 500]);
+        $this->assertEquals(500, $view->getStatusCode());
+        $this->assertTrue(
+            Str::contains($view->getContent(), 'savanna')
+        );
+
+        $app->flush();
+    }
+}
