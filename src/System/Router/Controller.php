4b63fb5543397bd244c2a2ab08cb28ab1fb41849
SonyPradana <sonypradana@gmail.com>
2023-06-25 18:01:50 +0700

    revent: add `renderView` and `viewExist`

--- src/System/Router/Controller.php
+++ src/System/Router/Controller.php
@@ -2,9 +2,14 @@
 
 namespace System\Router;
 
-/**
- * @deprecated
- */
+use System\Http\Response;
+
 abstract class Controller
 {
+    /**
+     * @param array<string, mixed> $portal
+     */
+    abstract public static function renderView(string $view, array $portal = []): Response;
+
+    abstract public static function viewExists(string $view): bool;
 }
