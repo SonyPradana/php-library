b4859b3ed04481e1dd32bdab5910af4bcc997c76
SonyPradana <sonypradana@gmail.com>
2023-06-25 10:57:51 +0700

    fix some class phpstan level 6

--- src/System/Router/Controller.php
+++ src/System/Router/Controller.php
@@ -2,30 +2,9 @@
 
 namespace System\Router;
 
+/**
+ * @deprecated
+ */
 abstract class Controller
 {
-    public function __invoke($invoke)
-    {
-        call_user_func([$this, $invoke]);
-    }
-
-    public static function renderView(string $view_path, array $portal = [])
-    {
-        // overwrite
-    }
-
-    /**
-     * @var static This classs
-     */
-    private self $_static;
-
-    /**
-     * Instance of controller.
-     * Shorthadn to crete new class.
-     */
-    public static function static()
-    {
-        /* @phpstan-ignore-next-line */
-        return self::$_static = self::$_static ?? new static();
-    }
 }
