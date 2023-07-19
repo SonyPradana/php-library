683dcb3cf45a838cd8beb66b732cbfa7d2a4d774
SonyPradana <sonypradana@gmail.com>
2023-07-19 13:01:49 +0700

    fix: `Schema::class` facade docblock

--- src/System/Support/Facades/Schema.php
+++ src/System/Support/Facades/Schema.php
@@ -7,7 +7,10 @@ namespace System\Support\Facades;
 /**
  * @method static \System\Database\MySchema\Create         create()
  * @method static \System\Database\MySchema\Drop           drop()
- * @method static \System\Database\MySchema\Table\Truncate refresh(string $database_name, string $table_name)
+ * @method static \System\Database\MySchema\Table\Truncate refresh(string $table_name)
+ * @method static \System\Database\MySchema\Table\Create   table(string $table_name, callable $blueprint)
+ * @method static \System\Database\MySchema\Table\Alter    alter(string $table_name, callable $blueprint)
+ * @method static \System\Database\MySchema\Table\Raw      raw(string $raw)
  */
 final class Schema extends Facade
 {
