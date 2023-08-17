b9beea7733476b6a7f3520faaedf00b675f1ebdc
SonyPradana <sonypradana@gmail.com>
2023-08-17 13:23:39 +0700

    refactor: deprecate some method and replace it
    - replace method name with camelCase
    - add new method `replace`

--- src/System/Console/Style/Style.php
+++ src/System/Console/Style/Style.php
@@ -338,13 +338,19 @@ class Style
     }
 
     /**
-     * Print terminal style.
-     *
-     * @return void
+     * Clear curent line (original text is keep).
      */
-    public function clear()
+    public function clear(): void
     {
-        $this->clear_line();
+        $this->clearLine();
+    }
+
+    /**
+     * Replace current line (original text is keep).
+     */
+    public function replace(string $text): void
+    {
+        $this->replaceLine($text);
     }
 
     // style ------------------------------------------
@@ -517,11 +523,25 @@ class Style
     /**
      * Push/insert new lines.
      *
+     * @deprecated
+     *
      * @param int $repeat
      *
      * @return self
      */
     public function new_lines($repeat = 1)
+    {
+        return $this->repeat("\n", $repeat);
+    }
+
+    /**
+     * Push/insert new lines.
+     *
+     * @param int $repeat
+     *
+     * @return self
+     */
+    public function newLines($repeat = 1)
     {
         return $this->repeat("\n", $repeat);
     }
