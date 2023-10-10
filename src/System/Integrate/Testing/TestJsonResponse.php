f0bae4c7cf8f3e0a39a945e78eabd3494dcda3b7
Angger Pradana <sonypradana@gmail.com>
2023-10-10 13:07:49 +0000

    add setter response data

--- src/System/Integrate/Testing/TestJsonResponse.php
+++ src/System/Integrate/Testing/TestJsonResponse.php
@@ -15,15 +15,27 @@ class TestJsonResponse extends TestResponse implements \ArrayAccess
     /**
      * @var array<string, mixed>
      */
-    private array $respone_data;
+    private array $response_data;
 
     public function __construct(Response $response)
     {
-        $this->response     = $response;
-        $this->respone_data = (array) $response->getContent();
+        $this->response      = $response;
+        $this->response_data = (array) $response->getContent();
         if (!is_array($response->getContent())) {
             throw new \Exception('Respone body is not Array.');
         }
+    }
+
+    /**
+     * Set response data.
+     *
+     * @param array<string, mixed> $response_data
+     */
+    public function setResponseData(array $response_data): self
+    {
+        $this->response_data = $response_data;
+
+        return $this;
     }
 
     /**
@@ -31,28 +43,28 @@ class TestJsonResponse extends TestResponse implements \ArrayAccess
      */
     public function getData()
     {
-        return $this->respone_data['data'];
+        return $this->response_data['data'];
     }
 
     public function offsetExists($offset): bool
     {
-        return array_key_exists($offset, $this->respone_data);
+        return array_key_exists($offset, $this->response_data);
     }
 
     #[\ReturnTypeWillChange]
     public function offsetGet($offset)
     {
-        return $this->respone_data[$offset];
+        return $this->response_data[$offset];
     }
 
     public function offsetSet($offset, $value): void
     {
-        $this->respone_data[$offset] = $value;
+        $this->response_data[$offset] = $value;
     }
 
     public function offsetUnset($offset): void
     {
-        unset($this->respone_data[$offset]);
+        unset($this->response_data[$offset]);
     }
 
     /**
@@ -60,43 +72,43 @@ class TestJsonResponse extends TestResponse implements \ArrayAccess
      */
     public function assertEqual(string $data_key, $value): void
     {
-        $data_get = data_get($this->respone_data, $data_key);
+        $data_get = data_get($this->response_data, $data_key);
         Assert::assertEquals($data_get, $value);
     }
 
     public function assertTrue(string $data_key, string $message = ''): void
     {
-        $data_get = data_get($this->respone_data, $data_key);
+        $data_get = data_get($this->response_data, $data_key);
         Assert::assertTrue($data_get, $message);
     }
 
     public function assertFalse(string $data_key, string $message = ''): void
     {
-        $data_get = data_get($this->respone_data, $data_key);
+        $data_get = data_get($this->response_data, $data_key);
         Assert::assertFalse($data_get, $message);
     }
 
     public function assertNull(string $data_key, string $message = ''): void
     {
-        $data_get = data_get($this->respone_data, $data_key);
+        $data_get = data_get($this->response_data, $data_key);
         Assert::assertNull($data_get, $message);
     }
 
     public function assertNotNull(string $data_key, string $message = ''): void
     {
-        $data_get = data_get($this->respone_data, $data_key);
+        $data_get = data_get($this->response_data, $data_key);
         Assert::assertNotNull($data_get, $message);
     }
 
     public function assertEmpty(string $data_key): void
     {
-        $data_get = data_get($this->respone_data, $data_key);
+        $data_get = data_get($this->response_data, $data_key);
         Assert::assertEmpty($this->getData());
     }
 
     public function assertNotEmpty(string $data_key): void
     {
-        $data_get = data_get($this->respone_data, $data_key);
+        $data_get = data_get($this->response_data, $data_key);
         Assert::assertNotEmpty($this->getData());
     }
 }
