0316cae65d978bb772223ee112cb1abb75960cb9
SonyPradana <sonypradana@gmail.com>
2023-10-24 20:54:55 +0700

    feat: modify header directly

--- src/System/Http/HeaderCollection.php
+++ src/System/Http/HeaderCollection.php
@@ -34,4 +34,119 @@ class HeaderCollection extends Collection
 
         return $this->set(\trim($header_name), \trim($header_val));
     }
+
+    /**
+     * Add new heder value directly to exist header.
+     *
+     * @param array<int|string, string|string[]> $value
+     */
+    public function addDirective(string $header, $value): self
+    {
+        $items = $this->parseDirective($header);
+        foreach ($value as $key => $new_item) {
+            if (is_int($key)) {
+                $items[] = $new_item;
+                continue;
+            }
+            $items[$key] = $new_item;
+        }
+
+        return $this->set($header, $this->encodeToString($items));
+    }
+
+    /**
+     * Remove exits header directly.
+     */
+    public function removeDirective(string $header, string $item): self
+    {
+        $items     = $this->parseDirective($header);
+        $new_items = [];
+        foreach ($items as $key => $value) {
+            if ($key === $item) {
+                continue;
+            }
+            if ($value === $item) {
+                continue;
+            }
+            $new_items[$key] = $value;
+        }
+
+        return $this->set($header, $this->encodeToString($new_items));
+    }
+
+    /**
+     * Check header directive has item/key.
+     */
+    public function hasDirective(string $header, string $item): bool
+    {
+        $items = $this->parseDirective($header);
+        foreach ($items as $key => $value) {
+            if ($key === $item) {
+                return true;
+            }
+            if ($value === $item) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Parde header item to array.
+     *
+     * @return array<string|int, string|string[]>
+     */
+    private function parseDirective(string $key)
+    {
+        if (false === $this->has($key)) {
+            return [];
+        }
+
+        $header        = $this->get($key);
+        $pattern       = '/,\s*(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/';
+        $header_item   = preg_split($pattern, $header);
+
+        $result = [];
+        foreach ($header_item as $item) {
+            if (strpos($item, '=') !== false) {
+                $parts = explode('=', $item, 2);
+                $key   = trim($parts[0]);
+                $value = trim($parts[1]);
+                if (substr($value, 0, 1) == '"' && substr($value, -1) == '"') {
+                    $value = substr($value, 1, -1);
+                    $value = array_map('trim', explode(', ', $value));
+                }
+                $result[$key] = $value;
+                continue;
+            }
+            $result[] = $item;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Encode array data to header string.
+     *
+     * @param array<string|int, string|string[]> $data
+     */
+    private function encodeToString($data): string
+    {
+        $encodedString = '';
+
+        foreach ($data as $key => $value) {
+            if (is_int($key)) {
+                $encodedString .= $value . ', ';
+                continue;
+            }
+
+            if (is_array($value)) {
+                $value = '"' . implode(', ', $value) . '"';
+            }
+            $encodedString .= $key . '=' . $value . ', ';
+        }
+
+        return rtrim($encodedString, ', ');
+    }
 }
