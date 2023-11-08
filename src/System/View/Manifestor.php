c44bf1fabfd1f0fbd9923bca907677a55b52c3e0
SonyPradana <sonypradana@gmail.com>
2023-11-08 23:15:11 +0700

    feat: templator with manifet file check
    - manifestor, manifest file managger

---
+++ src/System/View/Manifestor.php
@@ -0,0 +1,174 @@
+<?php
+
+declare(strict_types=1);
+
+namespace System\View;
+
+class Manifestor
+{
+    private string $cache_path;
+    private string $manifest_name;
+    /** @var array<string, string[]> */
+    private static $cache_manifest = [];
+
+    public function __construct(string $cache_path, string $manifest_name = 'manifest.json')
+    {
+        $this->cache_path    = $cache_path;
+        $this->manifest_name = $manifest_name;
+    }
+
+    public function manifestFileName(): string
+    {
+        return $file_name = $this->cache_path . $this->manifest_name;
+    }
+
+    /**
+     * Get manifest data as array paired.
+     *
+     * @return array<string, string[]>
+     */
+    public function getManifest()
+    {
+        if (false === file_exists($file_name = $this->manifestFileName())) {
+            throw new \InvalidArgumentException("Manifest file name doest exist `{$file_name}`.");
+        }
+
+        $file = file_get_contents($file_name);
+
+        if (false === $file) {
+            throw new \Exception('Cant load manifet file.');
+        }
+
+        return self::$cache_manifest = (array) json_decode($file, true, 512, JSON_THROW_ON_ERROR);
+    }
+
+    /**
+     * Get cached manifest data.
+     *
+     * @return array<string, string[]>
+     */
+    public static function getCachedManifest(string $cache_path, string $manifest_name = 'manifest.json')
+    {
+        if ([] === self::$cache_manifest) {
+            (new self($cache_path, $manifest_name))->getManifest();
+        }
+
+        return self::$cache_manifest;
+    }
+
+    public static function flushCachedManifest(): void
+    {
+        self::$cache_manifest = [];
+    }
+
+    /**
+     * Put manifest as json data.
+     *
+     * @param array<string, string[]> $manifest
+     */
+    public function putManifest($manifest): bool
+    {
+        if (false === ($raw = json_encode($manifest, JSON_PRETTY_PRINT))) {
+            return false;
+        }
+
+        if (false === file_put_contents($this->manifestFileName(), $raw)) {
+            return false;
+        }
+
+        self::$cache_manifest = $manifest;
+
+        return true;
+    }
+
+    public function hasManifest(): bool
+    {
+        return file_exists($this->manifestFileName());
+    }
+
+    public function init(): void
+    {
+        $this->putManifest([]);
+    }
+
+    /**
+     * Get teplate dependency slot/include.
+     *
+     * @return string[]
+     */
+    public function getDependency(string $template_filename)
+    {
+        return $this->getManifest()[$template_filename] ?? [];
+    }
+
+    /**
+     * Remove template file from manifest file.
+     */
+    public function removeDependency(string $template_filename): void
+    {
+        $new_manifest = [];
+        foreach ($this->getManifest() as $template => $dependency) {
+            if ($template === $template_filename) {
+                continue;
+            }
+
+            $new_manifest[$template] = $dependency;
+        }
+        $this->putManifest($new_manifest);
+    }
+
+    /**
+     * Replace dependecy to new (overwrite).
+     *
+     * @param string[] $new_dependency
+     */
+    public function replaceDependency(string $template_filename, $new_dependency): void
+    {
+        $new_manifest = [];
+        foreach ($this->getManifest() as $template => $dependency) {
+            if ($template === $template_filename) {
+                $new_manifest[$template] = $new_dependency;
+                continue;
+            }
+
+            $new_manifest[$template] = $dependency;
+        }
+        $this->putManifest($new_manifest);
+    }
+
+    /**
+     * Check template file depency is uptode to newst template.
+     *
+     * @throws \Exception when dependency file not exist
+     */
+    public function isDependencyUptodate(string $template_filename, int $template_time = null): bool
+    {
+        if (false === \file_exists($this->cache_path . '/' . $template_filename . '.php')) {
+            throw new \Exception("Cache file `{$template_filename}.php` is not exist.");
+        }
+
+        if (null === $template_time) {
+            $template_time = \filemtime($this->cache_path . '/' . $template_filename . '.php');
+        }
+
+        foreach ($this->getDependency($template_filename) as $file) {
+            $check     = $this->cache_path . '/' . $file . '.php';
+            $file_time = \filemtime($check);
+
+            if (false === file_exists($check)) {
+                throw new \Exception("Cache file `{$file}.php` is not exist.");
+            }
+
+            if ($file_time > $template_time) {
+                return false;
+            }
+
+            // dependency tree (recursing)
+            if (array_key_exists($file, self::$cache_manifest)) {
+                return $this->isDependencyUptodate($file, $file_time);
+            }
+        }
+
+        return true;
+    }
+}
---
+++ tests/View/ManifestorTest.php
@@ -0,0 +1,166 @@
+<?php
+
+declare(strict_types=1);
+
+namespace System\Test\View;
+
+use PHPUnit\Framework\TestCase;
+use System\View\Manifestor;
+
+class ManifestorTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        file_put_contents(__DIR__ . '/caches/manifestor.test.json', '[]');
+    }
+
+    protected function tearDown(): void
+    {
+        Manifestor::flushCachedManifest();
+        if (file_exists(__DIR__ . '/caches/manifestor.test.json')) {
+            unlink(__DIR__ . '/caches/manifestor.test.json');
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function itCanGetManifestData()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches/', 'manifestor.test.json');
+
+        $this->assertEquals(__DIR__ . '/caches/manifestor.test.json', $manifest->manifestFileName());
+        $this->assertEquals([], $manifest->getManifest());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanGetCachedManifestData()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches/', 'manifestor.test.json');
+
+        $manifest->putManifest(['a' => ['b', 'c']]);
+        $this->assertEquals(['a' => ['b', 'c']], Manifestor::getCachedManifest(__DIR__ . '/caches/', 'manifestor.test.json'));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanGetCachedManifestDataNoCached()
+    {
+        $this->assertEquals([], Manifestor::getCachedManifest(__DIR__ . '/caches/', 'manifestor.test.json'));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanPutMaifestData()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches/', 'manifestor.test.json');
+
+        $manifest->putManifest(['a' => ['b', 'c']]);
+        $this->assertEquals(['a' => ['b', 'c']], $manifest->getManifest());
+    }
+
+    /**
+     * @test
+     */
+    public function itCheakHasManifest()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches/', 'manifestor.test.json');
+
+        $this->assertTrue($manifest->hasManifest());
+    }
+
+    /**
+     * @test
+     */
+    public function itGetDependecy()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches/', 'manifestor.test.json');
+
+        $manifest->putManifest(['a' => ['b', 'c']]);
+        $this->assertEquals(['b', 'c'], $manifest->getDependency('a'));
+    }
+
+    /**
+     * @test
+     */
+    public function itRemoveDependecy()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches/', 'manifestor.test.json');
+
+        $manifest->putManifest(['a' => ['b', 'c']]);
+        $this->assertEquals(['b', 'c'], $manifest->getDependency('a'));
+        $manifest->removeDependency('a');
+        $this->assertEquals([], $manifest->getDependency('a'));
+    }
+
+    /**
+     * @test
+     */
+    public function itReplaceDependecy()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches/', 'manifestor.test.json');
+
+        $manifest->putManifest(['a' => ['b', 'c']]);
+        $this->assertEquals(['b', 'c'], $manifest->getDependency('a'));
+        $manifest->replaceDependency('a', ['d', 'e']);
+        $this->assertEquals(['d', 'e'], $manifest->getDependency('a'));
+    }
+
+    /**
+     * @test
+     */
+    public function itCheckDepencyIsUpdate()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches/', 'manifestor.test.json');
+
+        $manifest->putManifest(['a' => ['b', 'c']]);
+        file_put_contents(__DIR__ . '/caches/a.php', 'a');
+        file_put_contents(__DIR__ . '/caches/b.php', 'b');
+        file_put_contents(__DIR__ . '/caches/c.php', 'c');
+        $this->assertTrue($manifest->isDependencyUptodate('a'));
+    }
+
+    /**
+     * @test
+     */
+    public function itCheckDepencyIsNotUpdate()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches_fixed/', 'manifestor.test.json');
+
+        $manifest->putManifest(['a' => ['b', 'c']]);
+        file_put_contents(__DIR__ . '/caches_fixed/b.php', now()->format('Y-m-d H:i'));
+        file_put_contents(__DIR__ . '/caches_fixed/c.php', now()->format('Y-m-d H:i'));
+        $this->assertFalse($manifest->isDependencyUptodate('a'));
+    }
+
+    /**
+     * @test
+     */
+    public function itCheckDepencyIsUpdateWithTree()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches/', 'manifestor.test.json');
+
+        $manifest->putManifest(['a' => ['b', 'c'], 'c' => ['d']]);
+        file_put_contents(__DIR__ . '/caches/a.php', 'a');
+        file_put_contents(__DIR__ . '/caches/b.php', 'b');
+        file_put_contents(__DIR__ . '/caches/c.php', 'c');
+        file_put_contents(__DIR__ . '/caches/d.php', 'd');
+        $this->assertTrue($manifest->isDependencyUptodate('a'));
+    }
+
+    /**
+     * @test
+     */
+    public function itCheckDepencyIsNotUpdateWithTree()
+    {
+        $manifest = new Manifestor(__DIR__ . '/caches_fixed/', 'manifestor.test.json');
+
+        $manifest->putManifest(['a' => ['d'], 'd' => ['e']]);
+        file_put_contents(__DIR__ . '/caches_fixed/e.php', now()->format('Y-m-d H:i'));
+        $this->assertFalse($manifest->isDependencyUptodate('a'));
+    }
+}
