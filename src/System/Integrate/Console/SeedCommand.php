f70243604e8b824ba89331ba6f0a06f3c2c7f383
Angger Pradana <sonypradana@gmail.com>
2023-12-23 12:23:16 +0700

    [Feat] Run seeder after migration command (#260)
    * feat: run seeder after running migration
    
    * refactor: default db seeder when class not defined
    
    * fix: prevent run seeder in dry-run mode

--- src/System/Integrate/Console/SeedCommand.php
+++ src/System/Integrate/Console/SeedCommand.php
@@ -87,6 +87,12 @@ class SeedCommand extends Command
             return 2;
         }
 
+        if (null !== $class && null !== $namespace) {
+            warn('Use only one class or namespace, be specific.')->out(false);
+
+            return 1;
+        }
+
         if (null === $class && null !== $namespace) {
             $class = $namespace;
         }
@@ -96,9 +102,7 @@ class SeedCommand extends Command
         }
 
         if (null === $class && null === $namespace) {
-            warn('command db:seed require --class or --name-space flag follow by class name.')->out(false);
-
-            return 1;
+            $class = 'Database\\Seeders\\DatabaseSeeder';
         }
 
         if (false === class_exists($class)) {
@@ -111,7 +115,7 @@ class SeedCommand extends Command
         try {
             app()->call([$class, 'run']);
 
-            ok('Success run seeder')->out(false);
+            ok('Success run seeder ' . $class)->out(false);
         } catch (\Throwable $th) {
             warn($th->getMessage())->out(false);
             $exit = 1;
