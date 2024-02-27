4914259336b4242cc7babe7bc56099b2e227257b
SonyPradana <sonypradana@gmail.com>
2024-02-27 07:43:15 +0700

    test: multy rows model test
    - Tests: 67, Assertions: 66, Skipped: 11

--- tests/DataBase/Model/BaseMultyModelTest.php
+++ tests/DataBase/Model/BaseMultyModelTest.php
@@ -4,16 +4,92 @@ declare(strict_types=1);
 
 namespace System\Test\Database\Model;
 
+use System\Database\MyModel\Model;
+use System\Database\MyQuery;
+use System\Database\MyQuery\Insert;
 use System\Test\Database\BaseConnection;
 
 final class BaseMultyModelTest extends BaseConnection
 {
     protected function setUp(): void
     {
+        $this->createConnection();
+        $this->createUserSchema();
+        $password = password_hash('password', PASSWORD_DEFAULT);
+        $this->createUser([
+            [
+                'user'     => 'nuno',
+                'password' => $password,
+                'stat'     => 90,
+            ],
+            [
+                'user'     => 'taylor',
+                'password' => $password,
+                'stat'     => 100,
+            ],
+            [
+                'user'     => 'pradana',
+                'password' => $password,
+                'stat'     => 80,
+            ],
+        ]);
     }
 
     protected function tearDown(): void
     {
+        $this->dropConnection();
+    }
+
+    public function users(bool $read = true): User
+    {
+        $user = new User($this->pdo, []);
+        $user->indentifer()->equal('user', 'taylor');
+        if ($read) {
+            $user->read();
+        }
+
+        return $user;
+    }
+
+    private function createProfileSchema(): bool
+    {
+        return $this
+           ->pdo
+           ->query('CREATE TABLE `profiles` (
+                `user`      varchar(32)  NOT NULL,
+                `name`      varchar(100) NOT NULL,
+                `gender`    varchar(10) NOT NULL,
+                PRIMARY KEY (`user`)
+            )')
+           ->execute();
+    }
+
+    private function createProfiles($profiles): bool
+    {
+        return (new Insert('profiles', $this->pdo))
+            ->rows($profiles)
+            ->execute();
+    }
+
+    private function createOrderSchema(): bool
+    {
+        return $this
+           ->pdo
+           ->query('CREATE TABLE `orders` (
+                `id`   varchar(3)  NOT NULL,
+                `user` varchar(32)  NOT NULL,
+                `name` varchar(100) NOT NULL,
+                `type` varchar(30) NOT NULL,
+                PRIMARY KEY (`id`)
+            )')
+           ->execute();
+    }
+
+    private function createOrders($orders): bool
+    {
+        return (new Insert('orders', $this->pdo))
+            ->rows($orders)
+            ->execute();
     }
 
     /**
@@ -23,7 +99,15 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanCreateData()
     {
-        $this->markTestSkipped('tdd');
+        $user = new User($this->pdo, [
+            [
+                'user'     => 'jesicca',
+                'password' => password_hash('password', PASSWORD_DEFAULT),
+                'stat'     => 50,
+            ],
+        ], [[]]);
+
+        $this->assertTrue($user->insert());
     }
 
     /**
@@ -33,7 +117,9 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanReadData()
     {
-        $this->markTestSkipped('tdd');
+        $user = new User($this->pdo, [[]], ['user' => ['taylor']]);
+
+        $this->assertTrue($user->read());
     }
 
     /**
@@ -43,7 +129,11 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanUpdateData()
     {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+
+        $user->setter('stat', 75);
+
+        $this->assertTrue($user->update());
     }
 
     /**
@@ -53,47 +143,8 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanDeleteData()
     {
-        $this->markTestSkipped('tdd');
-    }
-
-    /**
-     * @test
-     *
-     * @group database
-     */
-    public function itCanCheckisClean()
-    {
-        $this->markTestSkipped('tdd');
-    }
-
-    /**
-     * @test
-     *
-     * @group database
-     */
-    public function itCanCheckisDirty()
-    {
-        $this->markTestSkipped('tdd');
-    }
-
-    /**
-     * @test
-     *
-     * @group database
-     */
-    public function itCanGetChangeColumn()
-    {
-        $this->markTestSkipped('tdd');
-    }
-
-    /**
-     * @test
-     *
-     * @group database
-     */
-    public function itCanGetHiddeColumn()
-    {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+        $this->assertTrue($user->delete());
     }
 
     /**
@@ -103,7 +154,12 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanGetFirst()
     {
-        $this->markTestSkipped('tdd');
+        $users = $this->users();
+
+        $this->assertEquals([
+            'user' => 'taylor',
+            'stat' => 100,
+        ], $users->first());
     }
 
     /**
@@ -113,7 +169,18 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanGetHasOne()
     {
-        $this->markTestSkipped('tdd');
+        // profile
+        $profile = [
+            'user'   => 'taylor',
+            'name'   => 'taylor otwell',
+            'gender' => 'male',
+        ];
+        $this->createProfileSchema();
+        $this->createProfiles([$profile]);
+
+        $user   = $this->users();
+        $result = $user->hasOne('profiles', 'user');
+        $this->assertEquals($profile, $result->toArray());
     }
 
     /**
@@ -123,7 +190,97 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanGetHasMany()
     {
-        $this->markTestSkipped('tdd');
+        // order
+        $order = [
+            [
+                'id'     => '1',
+                'user'   => 'taylor',
+                'name'   => 'order 1',
+                'type'   => 'gadget',
+            ], [
+                'id'     => '3',
+                'user'   => 'taylor',
+                'name'   => 'order 2',
+                'type'   => 'gadget',
+            ],
+        ];
+        $this->createOrderSchema();
+        $this->createOrders($order);
+
+        $user   = $this->users();
+        $result = $user->hasMany('orders', 'user');
+        $this->assertEquals($order, $result->toArray());
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanCheckisClean()
+    {
+        $user = $this->users();
+        $this->assertTrue($user->isClean(), 'Check all column');
+        $this->assertTrue($user->isClean('stat'), 'Check spesifik column');
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanCheckisDirty()
+    {
+        $user = $this->users();
+        $user->setter('stat', 75);
+        $this->assertTrue($user->isDirty(), 'Check all column');
+        $this->assertTrue($user->isDirty('stat'), 'Check spesifik column');
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanGetChangeColumn()
+    {
+        $user = $this->users();
+        $this->assertEquals([], $user->changes(), 'original fresh data');
+        // modify
+        $user->setter('stat', 75);
+        $this->assertEquals([
+            'stat' => 75,
+        ], $user->changes(), 'change first column');
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanHiddeColumn()
+    {
+        $user = $this->users();
+
+        $this->assertArrayNotHasKey('password', $user->first(), 'password must hidden by stash');
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanConvertToArray()
+    {
+        $user = $this->users();
+
+        $this->assertEquals([
+            [
+                'user' => 'taylor',
+                'stat' => 100,
+            ],
+        ], $user->toArray());
+        $this->assertIsIterable($user);
     }
 
     // getter setter - should return firts query
@@ -135,7 +292,10 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanGetUsingGetterInColumn()
     {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+
+        $columns = (fn () => $this->{'columns'})->call($user);
+        $this->assertEquals($columns[0]['stat'], $user->getter('stat', 0));
     }
 
     /**
@@ -145,7 +305,23 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanSetUsingSetterterInColumn()
     {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+
+        $user->setter('stat', 80);
+        $columns = (fn () => $this->{'columns'})->call($user);
+        $this->assertEquals(80, $columns[0]['stat']);
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanCheckExist()
+    {
+        $user = $this->users();
+
+        $this->assertTrue($user->has('user'));
     }
 
     /**
@@ -155,7 +331,10 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanGetUsingMagicGetterInColumn()
     {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+
+        $columns = (fn () => $this->{'columns'})->call($user);
+        $this->assertEquals($columns[0]['stat'], $user->stat);
     }
 
     /**
@@ -165,7 +344,11 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanSetUsingMagicSetterterInColumn()
     {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+
+        $user->stat = 80;
+        $columns    = (fn () => $this->{'columns'})->call($user);
+        $this->assertEquals(80, $columns[0]['stat']);
     }
 
     // array access
@@ -177,7 +360,10 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanGetUsingArray()
     {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+
+        $columns = (fn () => $this->{'columns'})->call($user);
+        $this->assertEquals($columns[0]['stat'], $user['stat']);
     }
 
     /**
@@ -187,7 +373,11 @@ final class BaseMultyModelTest extends BaseConnection
      */
     public function itCanSetUsingArray()
     {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+
+        $user['stat'] = 80;
+        $columns      = (fn () => $this->{'columns'})->call($user);
+        $this->assertEquals(80, $columns[0]['stat']);
     }
 
     /**
@@ -195,19 +385,26 @@ final class BaseMultyModelTest extends BaseConnection
      *
      * @group database
      */
-    public function itCanCheckUsingArray()
+    public function itCanCheckUsingMagicIsset()
     {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+        $this->assertTrue(isset($user['user']));
     }
 
     /**
+     * Unset is not perform anythink.
+     *
      * @test
      *
      * @group database
      */
     public function itCanUnsetUsingArray()
     {
-        $this->markTestSkipped('tdd');
+        $user = $this->users();
+
+        unset($user['stat']);
+        $columns = (fn () => $this->{'columns'})->call($user);
+        $this->assertEquals(100, $columns[0]['stat']);
     }
 
     // still can get collection
@@ -218,6 +415,83 @@ final class BaseMultyModelTest extends BaseConnection
      * @group database
      */
     public function itCanGetCollection()
+    {
+        $user = $this->users();
+
+        $columns = (fn () => $this->{'columns'})->call($user);
+        $models  = $user->get()->toArray();
+
+        // tranform to column
+        $arr = [];
+        foreach ($models as $new) {
+            $arr[]= (fn () => $this->{'columns'})->call($new)[0];
+        }
+        $this->assertEquals($columns, $arr);
+    }
+
+    // find user by some condition (static)
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanFindUsingId()
+    {
+        $user = User::find('taylor', $this->pdo);
+
+        $this->assertTrue($user->has('user'));
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanFindUsingWhere()
+    {
+        $user = User::where('user = :user', [
+            'user' => 'taylor',
+        ], $this->pdo);
+
+        $this->assertTrue($user->has('user'));
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanFindUsingEqual()
+    {
+        $user = User::equal('user', 'taylor', $this->pdo);
+
+        $this->assertTrue($user->has('user'));
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanFindAll()
+    {
+        $users   = MyQuery::from('users', $this->pdo)->select()->get()->toArray();
+        $models  = User::all($this->pdo);
+
+        $map = array_map(fn (Model $model) => $model->toArray()[0], $models->toArray());
+
+        foreach ($users as $key => $user) {
+            $this->assertEquals($user['user'], $map[$key]['user']);
+        }
+    }
+
+    /**
+     * @test
+     *
+     * @group database
+     */
+    public function itCanFindOrCreate()
     {
         $this->markTestSkipped('tdd');
     }
