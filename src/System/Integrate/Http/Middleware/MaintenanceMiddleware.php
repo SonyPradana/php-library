10ddd0ca37ca919dd741d9a72eb5921494c7462f
Angger Pradana <sonypradana@gmail.com>
2024-04-06 05:45:15 +0000

    refactor: maintenance middleware and to karnel

--- src/System/Integrate/Http/Middleware/MaintenanceMiddleware.php
+++ src/System/Integrate/Http/Middleware/MaintenanceMiddleware.php
@@ -6,11 +6,33 @@ namespace System\Integrate\Http\Middleware;
 
 use System\Http\Request;
 use System\Http\Response;
+use System\Integrate\Application;
 
 class MaintenanceMiddleware
 {
+    protected Application $app;
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
     public function handle(Request $request, \Closure $next): Response
     {
+        if ($this->app->isDownMaintenanceMode()) {
+            $data = $this->app->getDownData();
+
+            if (isset($data['redirect'])) {
+                return redirect($data['redirect']);
+            }
+
+            if (isset($data['template'])) {
+                $header = isset($data['retry']) ? ['Retry-After' => $data['retry']] : [];
+
+                return new Response($data['template'], $data['status'] ?? 503, $header);
+            }
+        }
+
         return $next($request);
     }
 }
