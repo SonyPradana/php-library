ec787b23d4d52b14553840916447d879d555673b
SonyPradana <sonypradana@gmail.com>
2024-04-25 19:40:48 +0700

    feat: Container implements ArrayAccess

--- src/System/Container/Container.php
+++ src/System/Container/Container.php
@@ -6,6 +6,39 @@ namespace System\Container;
 
 use DI\Container as DIContainer;
 
-class Container extends DIContainer
+class Container extends DIContainer implements \ArrayAccess
 {
+    /**
+     * @param string $offset
+     */
+    public function offsetExists($offset): bool
+    {
+        return $this->has($offset);
+    }
+
+    /**
+     * @template T
+     *
+     * @param string|class-string<T> $offset entry name or a class name
+     *
+     * @return mixed|T
+     */
+    #[\ReturnTypeWillChange]
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * @param string                 $offset
+     * @param mixed|DefinitionHelper $value
+     */
+    public function offsetSet($offset, $value): void
+    {
+        $this->set($offset, $value);
+    }
+
+    public function offsetUnset($offset): void
+    {
+    }
 }
