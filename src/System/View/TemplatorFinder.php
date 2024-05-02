87287f9e5ef1f34a353557250802954478363f37
Angger Pradana <sonypradana@gmail.com>
2024-05-02 14:14:52 +0000

    feat: templator view file finder

---
+++ src/System/View/TemplatorFinder.php
@@ -0,0 +1,127 @@
+<?php
+
+declare(strict_types=1);
+
+namespace System\View;
+
+use System\View\Exceptions\ViewFileNotFound;
+
+class TemplatorFinder
+{
+    /**
+     * View file location has register.
+     *
+     * @var string[]
+     */
+    protected array $views = [];
+
+    /**
+     * Paths.
+     *
+     * @var string[]
+     */
+    protected array $paths;
+
+    /**
+     * Extetions.
+     *
+     * @var string[]
+     */
+    protected array $extensions = ['.temlate.php'];
+
+    /**
+     * Create new View Finder intance.
+     *
+     * @param string[] $paths
+     * @param string[] $extensions
+     */
+    public function __construct(array $paths, ?array $extensions = null)
+    {
+        $this->paths = $paths;
+        $this->extensions ??= $extensions;
+    }
+
+    /**
+     * Find file location by view_name given.
+     *
+     * @throws ViewFileNotFound
+     */
+    public function find(string $view_name): string
+    {
+        if (isset($this->views[$view_name])) {
+            return $this->views[$view_name];
+        }
+
+        return $this->views[] = $this->findInPath($view_name, $this->paths);
+    }
+
+    /**
+     * Check view name exist.
+     */
+    public function exists(string $view_name): bool
+    {
+        if (isset($this->views[$view_name])) {
+            return true;
+        }
+
+        foreach ($this->paths as $path) {
+            foreach ($this->extensions as $extenstion) {
+                if (file_exists($path . $view_name . $extenstion)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Find view name posible paths given.
+     *
+     * @param string[] $paths
+     *
+     * @throws ViewFileNotFound
+     */
+    public function findInPath(string $view_name, array $paths): string
+    {
+        foreach ($paths as $path) {
+            foreach ($this->extensions as $extenstion) {
+                if (file_exists($find = $path . $view_name . $extenstion)) {
+                    return $find;
+                }
+            }
+        }
+
+        throw new ViewFileNotFound($view_name);
+    }
+
+    /**
+     * Add path to posible path location.
+     */
+    public function addPath(string $path): self
+    {
+        if (!isset($this->paths[$path])) {
+            $this->paths[] = $path;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add exteention in first array.
+     */
+    public function addExctention(string $extension): self
+    {
+        array_unshift($this->extensions, $extension);
+
+        return $this;
+    }
+
+    /**
+     * Flush view register file location.
+     */
+    public function flush(): void
+    {
+        $this->views = [];
+    }
+}
