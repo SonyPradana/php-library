afb2ca3258ceff728ab0ae497691c3c162ac3a9b
Angger Pradana <sonypradana@gmail.com>
2024-05-07 13:24:53 +0700

    refactor: use alias regester in container alias (#315)
    * refactor: use alias regester in container alias
    
    * revent: accidentally pushing code from other branch

--- src/System/Container/Container.php
+++ src/System/Container/Container.php
@@ -12,6 +12,47 @@ use DI\Container as DIContainer;
 class Container extends DIContainer implements \ArrayAccess
 {
     /**
+     * Registed aliases entry container.
+     *
+     * @var array<string, string>
+     */
+    protected array $aliases = [];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function get(string $id): mixed
+    {
+        $id = $this->getAlias($id);
+
+        return parent::get($id);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param array<array-key, mixed> $parameters Optional parameters to use to build the entry. Use this to force
+     *                                            specific parameters to specific values. Parameters not defined in this
+     *                                            array will be resolved using the container.
+     */
+    public function make(string $name, array $parameters = []): mixed
+    {
+        $name = $this->getAlias($name);
+
+        return parent::make($name, $parameters);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function has(string $id): bool
+    {
+        $id = $this->getAlias($id);
+
+        return parent::has($id);
+    }
+
+    /**
      * Set entry alias conntainer.
      */
     public function alias(string $abstract, string $alias): void
@@ -19,7 +60,18 @@ class Container extends DIContainer implements \ArrayAccess
         if ($abstract === $alias) {
             throw new \Exception("{$abstract} is aliased to itself.");
         }
-        $this->set($abstract, $this->get($alias));
+
+        $this->aliases[$alias] = $abstract;
+    }
+
+    /**
+     * Get alias for an abstract if available.
+     */
+    public function getAlias(string $abstract): string
+    {
+        return array_key_exists($abstract, $this->aliases)
+            ? $this->getAlias($this->aliases[$abstract])
+            : $abstract;
     }
 
     /**
