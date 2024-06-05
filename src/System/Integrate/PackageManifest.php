ae26baa913dad57660056730254ba3e6113531b2
Angger Pradana <sonypradana@gmail.com>
2024-06-05 20:14:41 +0700

    feat: added package manifest (#337)
    * feat: added package manifest
    
    - package manifest for composer installed package
    
    * add default providers place header

---
+++ src/System/Integrate/PackageManifest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace System\Integrate;
+
+final class PackageManifest
+{
+    /**
+     * Cached package manifest.
+     *
+     * @var array<string, array<string, array<int, string>>>|null
+     */
+    public ?array $package_manifest = null;
+
+    public function __construct(
+        private string $base_path,
+        private string $application_cache_path,
+        private ?string $vendor_path = null
+    ) {
+        $this->vendor_path ??= DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'composer' . DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * Get provider in cache package manifest.
+     *
+     * @return string[]
+     */
+    public function providers(): array
+    {
+        return $this->config()['providers'];
+    }
+
+    /**
+     * Get array of provider..
+     *
+     * @return array<string, string[]>
+     */
+    protected function config(): array
+    {
+        $this->getPackageManifest();
+        $entry = [
+            'providers' => [],
+        ];
+        foreach ($this->package_manifest ?? [] as $package) {
+            foreach ($package['providers'] ?? [] as $provider) {
+                $entry['providers'][] = $provider;
+            }
+        }
+
+        return $entry;
+    }
+
+    /**
+     * Get cached package manifest has been build.
+     *
+     * @return array<string, array<string, array<int, string>>>
+     */
+    protected function getPackageManifest(): array
+    {
+        if ($this->package_manifest) {
+            return $this->package_manifest;
+        }
+
+        if (false === file_exists($this->application_cache_path . 'provider.php')) {
+            $this->build();
+        }
+
+        return $this->package_manifest = require $this->application_cache_path . 'provider.php';
+    }
+
+    /**
+     * Build cache package manifest from composer installed package.
+     */
+    public function build(): void
+    {
+        $packages = [];
+        $provider = [];
+
+        // vendor\composer\installed.json
+        if (file_exists($file = $this->base_path . $this->vendor_path . 'installed.json')) {
+            $installed = file_get_contents($file);
+            $installed = json_decode($installed, true);
+
+            $packages = $installed['packages'] ?? [];
+        }
+
+        foreach ($packages as $package) {
+            $provider[$package['name']] = $package['extra']['savanna'] ?? [''];
+        }
+        array_filter($provider);
+
+        file_put_contents($this->application_cache_path . 'provider.php', '<?php return ' . var_export($provider, true) . ';' . PHP_EOL);
+    }
+}
