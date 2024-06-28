984924de0b7627fa4fea3c8494f26b0d1dd0becb
Angger Pradana <sonypradana@gmail.com>
2024-06-28 22:11:06 +0700

    feat: schedule facade (register schedule using register container) (#353)
    * feat: schedule facade (register schedule using register container)
    
    * formatting

---
+++ src/System/Support/Facades/Schedule.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace System\Support\Facades;
+
+/**
+ * @method static \System\Cron\ScheduleTime[] getPools()
+ * @method static \System\Cron\ScheduleTime   call(\Closure $call_back, array $params = [])
+ * @method static void                        execute()
+ * @method static void                        setLogger()
+ * @method static \System\Cron\Schedule       add(\System\Cron\Schedule $schedule)
+ * @method static void                        flush()
+ */
+final class Schedule extends Facade
+{
+    protected static function getAccessor()
+    {
+        return 'schedule';
+    }
+}
--- tests/Cron/BasicCronTest.php
+++ tests/Cron/BasicCronTest.php
@@ -68,4 +68,33 @@ final class BasicCronTest extends TestCase
             }
         }
     }
+
+    /**
+     * @test
+     */
+    public function itCanAddSchedule(): void
+    {
+        $cron1 = new Schedule(now()->timestamp, $this->logger);
+        $cron1->call(fn (): bool => true)->eventName('from1');
+        $cron2 = new Schedule(now()->timestamp, $this->logger);
+        $cron2->call(fn (): bool => true)->eventName('from2');
+        $cron1->add($cron2);
+
+        $this->assertEquals('from1', $cron1->getPools()[0]->getEventname());
+        $this->assertEquals('from2', $cron1->getPools()[1]->getEventname());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanFlush(): void
+    {
+        $cron = new Schedule(now()->timestamp, $this->logger);
+        $cron->call(fn (): bool => true)->eventName('one');
+        $cron->call(fn (): bool => true)->eventName('two');
+
+        $this->assertCount(2, $cron->getPools());
+        $cron->flush();
+        $this->assertCount(0, $cron->getPools());
+    }
 }
