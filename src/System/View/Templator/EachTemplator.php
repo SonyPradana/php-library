7c600c7f5002d81590e22ff946fbfc0fb281018d
sonypradana <sonypradana@gmail.com>
2025-04-12 04:58:28 +0700

    fix: fix foreach syntax in EachTest

--- src/System/View/Templator/EachTemplator.php
+++ src/System/View/Templator/EachTemplator.php
@@ -11,14 +11,20 @@ class EachTemplator extends AbstractTemplatorParse
     public function parse(string $template): string
     {
         $template = preg_replace(
-            '/{%\s*foreach\s+([^%]+)\s+as\s+([^%]+)\s*=>\s*([^%]+)\s*%}(.*?){%\s*endforeach\s*%}/s',
-            '<?php foreach ($1 as $2 => $3): ?>$4<?php endforeach; ?>',
+            '/{%\s*foreach\s+([^%]+)\s+as\s+([^%]+)\s*=>\s*([^%]+)\s*%}/s',
+            '<?php foreach ($1 as $2 => $3): ?>',
             $template
         );
 
         $template = preg_replace(
-            '/{%\s*foreach\s+([^%]+)\s+as\s+([^%]+)\s*%}(.*?){%\s*endforeach\s*%}/s',
-            '<?php foreach ($1 as $2): ?>$3<?php endforeach; ?>',
+            '/{%\s*foreach\s+([^%]+)\s+as\s+([^%]+)\s*%}/s',
+            '<?php foreach ($1 as $2): ?>',
+            $template
+        );
+
+        $template = preg_replace(
+            '/{%\s*endforeach\s*%}/s',
+            '<?php endforeach; ?>',
             $template
         );
 
