e4acf0144eaff864ee11f52be14a0003625d203f
sonypradana <sonypradana@gmail.com>
2025-09-25 15:18:50 +0700

    WIP

--- src/System/Database/MyPDO.php
+++ src/System/Database/MyPDO.php
@@ -151,7 +151,7 @@ class MyPDO
         $message = $e->getMessage();
 
         foreach ($errors as $error) {
-            if (false === str_contains($message, $error)) {
+            if (true === str_contains($message, $error)) {
                 return true;
             }
         }
