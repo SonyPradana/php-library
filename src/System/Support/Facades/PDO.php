5cfe507590670e748e88c5054f92bd9c1c593cef
sonypradana <sonypradana@gmail.com>
2025-10-16 16:24:19 +0700

    wip

--- src/System/Support/Facades/PDO.php
+++ src/System/Support/Facades/PDO.php
@@ -5,34 +5,30 @@ declare(strict_types=1);
 namespace System\Support\Facades;
 
 /**
- * @method static void                                            clearConnections()
- * @method static \System\Database\Interfaces\ConnectionInterface connection(string $name)
- * @method static \System\Database\DatabaseManager                setDefaultConnection(\System\Database\Interfaces\ConnectionInterface $connection)
- * @method static \System\Database\DatabaseManager                query(string $query)
- * @method static \System\Database\DatabaseManager                bind(string|int|bool|null $param, mixed $value, string|int|bool|null $type = null)
- * @method static bool                                            execute()
- * @method static mixed[]|false                                   resultset()
- * @method static mixed                                           single()
- * @method static int                                             rowCount()
- * @method static bool                                            transaction(callable $callable)
- * @method static bool                                            beginTransaction()
- * @method static bool                                            endTransaction()
- * @method static bool                                            cancelTransaction()
- * @method static string|false                                    lastInsertId()
- * @method static void                                            flushLogs()
- * @method static array<int, array<string, float|string|null>>    getLogs()
+ * @method static \System\Database\MyPDO                                                                                                                                            instance()
+ * @method static MyPDO                                                                                                                                                             conn(array<string, string> $configs)
+ * @method static array{driver: string, host: ?string, database: ?string, port: ?int, charset: ?string, username: ?string, password: ?string, options: array<int, string|int|bool>} configs()
+ * @method static string                                                                                                                                                            getDsn(array{host: string, driver: 'mysql'|'mariadb'|'pgsql'|'sqlite', database: ?string, port: ?int, charset: ?string} $configs)
+ * @method static \System\Database\MyPDO                                                                                                                                            query(string $query)
+ * @method static \System\Database\MyPDO                                                                                                                                            bind(string|int|bool|null $param, mixed $value, string|int|bool|null $type = null)
+ * @method static bool                                                                                                                                                              execute()
+ * @method static array|false                                                                                                                                                       resultset()
+ * @method static mixed                                                                                                                                                             single()
+ * @method static int                                                                                                                                                               rowCount()
+ * @method static string|false                                                                                                                                                      lastInsertId()
+ * @method static bool                                                                                                                                                              transaction(callable $callable)
+ * @method static bool                                                                                                                                                              beginTransaction()
+ * @method static bool                                                                                                                                                              endTransaction()
+ * @method static bool                                                                                                                                                              cancelTransaction()
+ * @method static void                                                                                                                                                              flushLogs()
+ * @method static array                                                                                                                                                             getLogs()
  *
- * @see System\Database\DatabaseManager
+ * @see System\Database\MyPDO
  */
 final class PDO extends Facade
 {
     protected static function getAccessor()
     {
         return \System\Database\DatabaseManager::class;
-    }
-
-    public static function instance(): \System\Database\DatabaseManager
-    {
-        return self::getFacade();
     }
 }
